84b699a1fcd111c76b66c4f0cf4725447539bec5
Yousef Altaher <yousef.a.altaher@gmail.com>
2022-07-02 11:34:31 +0100

    small refactoring

--- assets/js/nodes.js
+++ assets/js/nodes.js
@@ -1,131 +1,132 @@
+//TODO Combine all these three
 const url = "https://masader-web-service.herokuapp.com/datasets";
-const urlEmb = "https://masader-web-service.herokuapp.com/datasets?features=Embeddings";
-const urlClus = "https://masader-web-service.herokuapp.com/datasets?features=Cluster";
+const urlEmbClus =
+    "https://masader-web-service.herokuapp.com/datasets?features=Cluster,Embeddings";
+
 function reformat_numbers(num) {
-    if (num === undefined)
-        return ''
-    values = num.split(',')
+    if (num === undefined) return "";
+    values = num.split(",");
     if (values.length < 2) {
-        return num
+        return num;
     } else if (values.length == 2) {
-        return values[0] + 'K'
-    } else
-        return values[0] + 'M'
+        return values[0] + "K";
+    } else return values[0] + "M";
 }
 
-function reformat_dialect(dialect)
-{
-    
-    if (dialect.trim() != 'other')
-    {
-        dialect = dialect.split(':')[0]
-        dialect = dialect.split('-')[1]
-    }
-    
-    return dialect
-}
-
-function reformat_tasks(tasks)
-{
-    let out_html = ''
-
-    tasks = tasks.split(',')
-    for(let j = 0 ; j < tasks.length ; j += 1){
-        out_html += tasks[j]+'</br>'
+function reformat_dialect(dialect) {
+    if (dialect.trim() != "other") {
+        dialect = dialect.split(":")[0];
+        dialect = dialect.split("-")[1];
     }
 
-    return out_html
+    return dialect;
 }
 
-function createHtml(i)
-{
-    let div = '<div style="font-family: Cairo, "Open Sans"> '
-    let table = '<table style="border-collapse: collapse; border: none;">'
-    let html_out = div + table 
-    let list_to_show = ['Name', 'Year','Dialect', 'Volume', 'Tasks']
-    for(let j = 0 ; j < list_to_show.length ; j += 1){
+function reformat_tasks(tasks) {
+    let out_html = "";
 
-        let index_to_header = headersWhiteList.indexOf(list_to_show[j])
-        let header = headersWhiteList[index_to_header]
-        let value = ' ' +dataset[i][index_to_header]
-        html_out += '<tr style="border: none;">'
-        html_out += '<td style="border: none;">'
-        html_out += '<b>'+header+'</b>'
-        html_out += '</td>'
-        html_out += '<td style="border: none;">'
-        
-        if(header == 'Volume')
-        {
-            html_out += reformat_numbers(value)+ ' '+ dataset[i][index_to_header+1]
-        }
-        else if (header == 'Name')
-        {
-            html_out += `<a href = "">${value}</a>`
-        }
-        else if (header == 'Dialect')
-        {
-            html_out += reformat_dialect(value)
-        }
-        else if (header == 'Tasks'){
-            html_out += reformat_tasks(value)
-        }
-        else
-        {
-            html_out += value
-        }
-        
-        html_out += '</td>'
-        html_out += '</tr>'
+    tasks = tasks.split(",");
+    for (let j = 0; j < tasks.length; j += 1) {
+        out_html += tasks[j] + "</br>";
     }
 
-    return html_out+'</table>'+'</div>'
+    return out_html;
 }
 
-const reteriveEmbeddings = async () => {
-  return await axios.get(urlEmb, ).then(function(response) {
-        let rowData = response.data
-                      .map( (e) => e.Embeddings)
-        return rowData;
+function createHtml(i) {
+    let div = '<div style="font-family: Cairo, "Open Sans"> ';
+    let table = '<table style="border-collapse: collapse; border: none;">';
+    let html_out = div + table;
+    let list_to_show = ["Name", "Year", "Dialect", "Volume", "Tasks"];
+    for (let j = 0; j < list_to_show.length; j += 1) {
+        let index_to_header = headersWhiteList.indexOf(list_to_show[j]);
+        let header = headersWhiteList[index_to_header];
+        let value = " " + dataset[i][index_to_header];
+        html_out += '<tr style="border: none;">';
+        html_out += '<td style="border: none;">';
+        html_out += "<b>" + header + "</b>";
+        html_out += "</td>";
+        html_out += '<td style="border: none;">';
+
+        if (header == "Volume") {
+            html_out +=
+                reformat_numbers(value) + " " + dataset[i][index_to_header + 1];
+        } else if (header == "Name") {
+            html_out += `<a href = "">${value}</a>`;
+        } else if (header == "Dialect") {
+            html_out += reformat_dialect(value);
+        } else if (header == "Tasks") {
+            html_out += reformat_tasks(value);
+        } else {
+            html_out += value;
+        }
+
+        html_out += "</td>";
+        html_out += "</tr>";
+    }
+
+    return html_out + "</table>" + "</div>";
+}
+
+const reteriveClustersEmbeddings = async () => {
+    return await axios.get(urlEmbClus).then(function (response) {
+        info = { embeddings: [], clusters: [] };
+        response.data.forEach((r) => {
+            info.embeddings.push(r.Embeddings);
+            info.clusters.push(r.Cluster);
+        });
+        console.log(info);
+        return info;
     });
-}
+};
 
-const reteriveClusters = async () => {
-    return await axios.get(urlClus, ).then(function(response) {
-        let rowData = response.data
-                      .map( (c) => c.Cluster)
-        return rowData;
-    });
-}
+axios
+    .get(url)
+    .then(async function (response) {
+        let rowData = response.data;
 
-axios.get(url, ).then(async function(response) {
-    let rowData = response.data
+        headersWhiteList = [
+            "Name",
+            "Link",
+            "License",
+            "Year",
+            "Language",
+            "Dialect",
+            "Domain",
+            "Form",
+            "Volume",
+            "Unit",
+            "Ethical Risks",
+            "Script",
+            "Access",
+            "Tasks",
+            "Venue Type",
+        ];
+        $(".loading-spinner").hide();
 
-    headersWhiteList = ['Name', 'Link', 'License', 'Year', 'Language', 'Dialect', 'Domain', 'Form', 'Volume', 'Unit', 'Ethical Risks', 'Script', 'Access', 'Tasks', 'Venue Type']
-    $('.loading-spinner').hide()
+        // Grabbing row's values
+        dataset = [];
+        for (let i = 0; i < rowData.length; i++) {
+            record = {};
+            for (let j = 0; j < headersWhiteList.length; j++)
+                record[j] = rowData[i][headersWhiteList[j]];
 
-    // Grabbing row's values
-    dataset = []
-    for (let i = 0; i < rowData.length; i++) {
+            dataset.push(record);
+        }
 
-        t = {}
-        for (let j = 0; j < headersWhiteList.length; j++)
-            t[j] = rowData[i][headersWhiteList[j]];
+        const info = await reteriveClustersEmbeddings();
+        var embeddings = info.embeddings;
+        var clusters = info.clusters;
 
-        dataset.push(t);
-    }
+        let box = document.querySelector(".box");
+        const width = box.offsetWidth;
+        const height = 500;
 
-        var embeddings = await reteriveEmbeddings();
-        var clusters = await reteriveClusters();
-        
-            let box = document.querySelector('.box');
-            const width = box.offsetWidth;
-            const height = 500;
+        var svg = d3.select("svg");
+        var dimension = document.body.getBoundingClientRect();
 
-            var svg = d3.select('svg');  
-            var dimension = document.body
-                .getBoundingClientRect();
-            
-            var tooltip = d3.select("body")
+        var tooltip = d3
+            .select("body")
             .append("div")
             .style("position", "absolute")
             .style("background-color", "white")
@@ -134,103 +135,105 @@ axios.get(url, ).then(async function(response) {
             .style("border-radius", "5px")
             .style("padding", "10px")
             .style("z-index", "10")
-            .style("visibility", "hidden")
-            ;
-            var data = d3.range(0, embeddings.length).map(function (d) {
-                return {
+            .style("visibility", "hidden");
+        var data = d3.range(0, embeddings.length).map(function (d) {
+            return {
                 x: embeddings[d][1],
-                y: embeddings[d][0]
-              }
-            });
+                y: embeddings[d][0],
+            };
+        });
 
-            var x = d3.scaleLinear()
-            .domain([-3, 55])
-            .range([ 0, width ]);
+        var x = d3.scaleLinear().domain([-3, 55]).range([0, width]);
 
-            // Add Y axis
-            var y = d3.scaleLinear()
-                .domain([0, 55])
-                .range([ height, 0]);
+        // Add Y axis
+        var y = d3.scaleLinear().domain([0, 55]).range([height, 0]);
 
-            
-            var zoom = d3.zoom()
-                .scaleExtent([.5, 10])  // This control how much you can unzoom (x0.5) and zoom (x20)
-                .extent([[0, 0], [width, height]])
-                .on("zoom", updateChart);
-          
-            var svg = d3.select("svg")
-                .attr('width', width)
-                .attr('height', height)
-                .style('background', 'white')
-                .attr("style", "outline: thin solid gray;")
-                .call(zoom)                       // Adds zoom functionality
+        var zoom = d3
+            .zoom()
+            .scaleExtent([0.5, 10]) // This control how much you can unzoom (x0.5) and zoom (x20)
+            .extent([
+                [0, 0],
+                [width, height],
+            ])
+            .on("zoom", updateChart);
 
-            var canvas = svg
-                .append("g")
-                .attr("class", "zoomable")
-            
-              function updateChart() {
-                if (canvas) {
-                    canvas.attr("transform", d3.event.transform);
-                    // recover the new scale
-                    var newX = d3.event.transform.rescaleX(x);
-                    var newY = d3.event.transform.rescaleY(y);
-                
-                
-                    // update circle position
-                    canvas.selectAll("circle")
-                        .attr('cx', function(d) {return newX(d.x)})
-                        .attr('cy', function(d) {return newY(d.y)});
+        var svg = d3
+            .select("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .style("background", "white")
+            .attr("style", "outline: thin solid gray;")
+            .call(zoom); // Adds zoom functionality
+
+        var canvas = svg.append("g").attr("class", "zoomable");
+
+        function updateChart() {
+            if (canvas) {
+                canvas.attr("transform", d3.event.transform);
+                // recover the new scale
+                var newX = d3.event.transform.rescaleX(x);
+                var newY = d3.event.transform.rescaleY(y);
+
+                // update circle position
+                canvas
+                    .selectAll("circle")
+                    .attr("cx", function (d) {
+                        return newX(d.x);
+                    })
+                    .attr("cy", function (d) {
+                        return newY(d.y);
+                    });
+            }
+        }
+
+        canvas
+            .selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("r", function (_, i, n) {
+                let vol_index = headersWhiteList.indexOf("Volume");
+                try {
+                    let volume = parseInt(dataset[i][vol_index].replaceAll(",", ""));
+                    if (isNaN(volume)) {
+                        return 10;
+                    } else return Math.log(volume);
+                } catch (err) {
+                    return 10;
                 }
-              }
-
-            canvas.selectAll('circle')
-                .data(data)
-                .enter()
-                .append('circle')
-                .attr("r", function(_, i, n){
-                    let vol_index = headersWhiteList.indexOf('Volume')
-                    try{
-                        let volume = parseInt(dataset[i][vol_index].replaceAll(",",""))
-                        if (isNaN(volume)){
-                            return 10
-                        }
-                        else
-                            return Math.log(volume)
-                    }catch(err){
-                        return 10
-                    }
-                    
-                }) 
-                .attr('opacity', 0.8)
-                .attr('cx', function(d) {
-                    return x(d.x);})
-                .attr('cy', function(d) {
-                    return y(d.y)})
-                .style('fill', function(_, i, n) {
-                    const index = clusters[i]
-                    return d3.schemeCategory20[index]
-                }).on("mouseover", function(_, i, n){                    
-                    tooltip = tooltip.html(createHtml(i));
-                    d3.select(this).style('stroke', '#eaeaea')
-                    d3.select(this).style('stroke-width', '5')
-                    return tooltip.style("visibility", "visible");
-                })
-                .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
-                .on("mouseout", function(){
-                    d3.select(this).style('stroke', 'white')
-                    d3.select(this).style('stroke-width', '0')
-                    return tooltip.style("visibility", "hidden");
-                })
-                .on("click", function(_, i, n){
-                    let url = 'card.html?'+i;
-                    window.open(url, '_blank').focus();
-                });
-
-
+            })
+            .attr("opacity", 0.8)
+            .attr("cx", function (d) {
+                return x(d.x);
+            })
+            .attr("cy", function (d) {
+                return y(d.y);
+            })
+            .style("fill", function (_, i, n) {
+                const index = clusters[i];
+                return d3.schemeCategory20[index];
+            })
+            .on("mouseover", function (_, i, n) {
+                tooltip = tooltip.html(createHtml(i));
+                d3.select(this).style("stroke", "#eaeaea");
+                d3.select(this).style("stroke-width", "5");
+                return tooltip.style("visibility", "visible");
+            })
+            .on("mousemove", function () {
+                return tooltip
+                    .style("top", event.pageY - 10 + "px")
+                    .style("left", event.pageX + 10 + "px");
+            })
+            .on("mouseout", function () {
+                d3.select(this).style("stroke", "white");
+                d3.select(this).style("stroke-width", "0");
+                return tooltip.style("visibility", "hidden");
+            })
+            .on("click", function (_, i, n) {
+                let url = "card.html?" + i;
+                window.open(url, "_blank").focus();
+            });
     })
-    .catch(function(error) {
+    .catch(function (error) {
         console.log(error);
     });
-
-            
