4631c8a7c4ecc1df2ddee35cc1c8242df7e765af
Yousef Altaher <yousef.a.altaher@gmail.com>
2022-07-10 14:14:05 +0100

    Fix NAN in paper link

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 const url = "https://masader-web-service.herokuapp.com/datasets";
 
 function linkuize(text, link) {
-    if(link != undefined || link != "nan")
+    if(link != undefined && link != "nan")
         return `<a href = "${link}" target="_blank"> ${text}</a>`
     else
         return ""
