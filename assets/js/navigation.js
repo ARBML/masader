415f84ed5d56c006fcd441533941db235c999c7a
unifoliatus <unifoliatus@outlook.com>
2022-07-13 15:30:56 +0300

    feat(navigation): :sparkles: add navigation menu for mobile
    mobile users can now navigate through the website using toggable navigation menu

---
+++ assets/js/navigation.js
@@ -0,0 +1,11 @@
+/**
+ * @type {HTMLUListElement}
+ */
+const menu = document.querySelector(".navigation-mobile-menu");
+
+/**
+ * @type {HTMLLIElement}
+ */
+const toggle = document.querySelector(".navigation-mobile-menu-toggle");
+
+toggle.addEventListener("click", (e) => menu.classList.toggle("hidden"));
