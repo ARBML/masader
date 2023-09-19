eb77781b6ede779c4e13bb0f7ccf6a0f5797be8e
zaidalyafeai <alyafey22@gmail.com>
2023-09-19 13:29:49 +0300

    allow adding exta spaces to name search.

--- assets/js/search.js
+++ assets/js/search.js
@@ -139,7 +139,7 @@ form.addEventListener("submit", (event) => {
       const parameter = new URLSearchParams({
         query: [
           ...(isProvided("name")
-            ? [`Name.str.contains('(?i)${queries.get("name")}')`]
+            ? [`Name.str.contains('(?i)${queries.get("name").trim()}')`]
             : []),
           ...(isProvided("dialect") ? [`Dialect.str.contains('(?i)${queries.get("dialect")}')`] : []),
           ...(isProvided("license") ? [`License.str.contains('(?i)${queries.get("license")}')`] : []),
